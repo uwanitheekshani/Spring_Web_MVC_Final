a5278b706c077876b0caae25592235c23abc80eb
uwanitheekshani <uwanitheekshani@gmail.com>
2023-02-15 10:12:42 +0530

    create JPAConfig

--- Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -21,39 +21,39 @@ import javax.sql.DataSource;
 //@EnableJpaRepositories(basePackageClasses = {CustomerRepo.class})//link dao classes
 public class JPAConfig {
 
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
-//        LocalContainerEntityManagerFactoryBean bean=new LocalContainerEntityManagerFactoryBean();
-//        bean.setPackagesToScan("lk.ijse.spring.entity");
-//        bean.setDataSource(ds);
-//        bean.setJpaVendorAdapter(jpa);
-//        return bean;
-//    }
-//
-//    @Bean
-//    public DataSource dataSource(){
-//        //we use this data source only for testing purpose(Development)
-//        //if e are in (Production) we can use a DBCP pool
-//        DriverManagerDataSource ds =new DriverManagerDataSource();
-//        ds.setDriverClassName("com.mysql.jdbc.Driver");
-//        ds.setUrl("jdbc:mysql://localhost:3306/spring_jpa?createDatabaseIfNotExist=true");
-//        ds.setUsername("root");
-//        ds.setPassword("1234");
-//        return ds;
-//    }
-//    @Bean
-//    public JpaVendorAdapter jpaVendorAdapter(){
-//        HibernateJpaVendorAdapter va=new HibernateJpaVendorAdapter();
-//        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-//        va.setDatabase(Database.MYSQL);
-//        va.setGenerateDdl(true);
-//        va.setShowSql(true);
-//        return va;
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-//        return new JpaTransactionManager(emf);
-//    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
+        LocalContainerEntityManagerFactoryBean bean=new LocalContainerEntityManagerFactoryBean();
+        bean.setPackagesToScan("lk.ijse.spring.entity");
+        bean.setDataSource(ds);
+        bean.setJpaVendorAdapter(jpa);
+        return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        //we use this data source only for testing purpose(Development)
+        //if e are in (Production) we can use a DBCP pool
+        DriverManagerDataSource ds =new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/car_rental?createDatabaseIfNotExist=true");
+        ds.setUsername("root");
+        ds.setPassword("1234");
+        return ds;
+    }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va=new HibernateJpaVendorAdapter();
+        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setShowSql(true);
+        return va;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
 
 }
