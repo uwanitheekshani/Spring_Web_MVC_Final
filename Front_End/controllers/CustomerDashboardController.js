c65850f42f6d481b8c71e5546b5e578fb85b03c7
uwanitheekshani <uwanitheekshani@gmail.com>
2023-02-19 12:56:58 +0530

    create customerdashboardcontroller

---
+++ Front_End/controllers/CustomerDashboardController.js
@@ -0,0 +1,29 @@
+$("#home").click(function () {
+    $("#headContent").css('display','block');
+    $("#main3").css('display','block');
+    $("#foot").css('display','block');
+    $("#storeContent").css('display','none');
+    $("#checkOutContent").css('display','none');
+    $("#accountContent").css('display','none');
+});
+
+// $("#account").click(function () {
+//     $("#headContent").css('display','none');
+//     $("#storeContent").css('display','none');
+//     $("#checkOutContent").css('display','none');
+//     $("#accountContent").css('display','block');
+// });
+//
+// $("#item").click(function () {
+//     $("#dashboardContent").css('display','none');
+//     $("#customerContent").css('display','none');
+//     $("#itemContent").css('display','block');
+//     $("#orderContent").css('display','none');
+// });
+//
+// $("#order").click(function () {
+//     $("#dashboardContent").css('display','none');
+//     $("#customerContent").css('display','none');
+//     $("#itemContent").css('display','none');
+//     $("#orderContent").css('display','block');
+// });
