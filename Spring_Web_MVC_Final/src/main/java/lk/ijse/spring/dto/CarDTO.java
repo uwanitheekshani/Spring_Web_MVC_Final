c6341039f5bf53bc0f6880d0aa0df66f1793a0e6
uwanitheekshani <uwanitheekshani@gmail.com>
2023-02-22 17:21:22 +0530

    create car dto

--- Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/dto/CarDTO.java
+++ Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -1,4 +1,22 @@
 package lk.ijse.spring.dto;
 
 public class CarDTO {
+    private String registrationId;
+    private String Brand;
+    private String type;
+    private String model;
+    private String fuelType;
+    private String transmissionType;
+    private String color;
+    private int noOfPassengers;
+    private String image_1;
+    private String image_2;
+    private String image_3;
+    private String image_4;
+    private long lastServiceMileage;
+    private long freeMileage;
+    private double dailyRate;
+    private double monthlyRate;
+    private double priceForExtraKm;
+    private String availability;
 }
