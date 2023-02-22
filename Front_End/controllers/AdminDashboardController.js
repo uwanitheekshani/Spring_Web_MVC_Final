a37cf5181efa45410c10a60fe3e92fe37b2c8cf5
uwanitheekshani <uwanitheekshani@gmail.com>
2023-02-22 15:51:46 +0530

    content update

---
+++ Front_End/controllers/AdminDashboardController.js
@@ -0,0 +1,12 @@
+// $("#home").click(function () {
+//     $("#mainh").css('display','block');
+//     $("#main3").css('display','block');
+//     $("#foot").css('display','block');
+//     $("#storeContent").css('display','none');
+//     $("#checkOutContent").css('display','none');
+//     $("#accountContent").css('display','none');
+//     $("#loginContent").css('display','none');
+//     $("#cusRegiContent").css('display','none');
+//     $("#cusUpdateContent").css('display','none');
+//
+// });
