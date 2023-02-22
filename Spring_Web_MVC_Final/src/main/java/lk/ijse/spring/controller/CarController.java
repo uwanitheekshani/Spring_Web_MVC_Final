e3f7655ced4dd9dc02f6eca9ab0da2a714c5d0fa
uwanitheekshani <uwanitheekshani@gmail.com>
2023-02-22 17:29:53 +0530

    add autowired annotation to the customer controller

--- Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/controller/CarController.java
+++ Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/car")
 @CrossOrigin
 public class CarController {
+
+    @Autowired
+    CarService service;
+
 }
