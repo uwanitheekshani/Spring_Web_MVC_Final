1ec497d8b365a04754febce36b454b1377a4d5ff
uwanitheekshani <uwanitheekshani@gmail.com>
2023-02-23 15:32:41 +0530

    added driver post mapping codes

--- Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ Spring_Web_MVC_Final/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.AdminService;
+import lk.ijse.spring.service.DriverService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/driver")
+@CrossOrigin
 public class DriverController {
+
+    @Autowired
+    DriverService service;
+
+
+    @PostMapping
+    public ResponseUtil saveDriver(@RequestBody DriverDTO dto){
+        service.saveDriver(dto);
+        return new ResponseUtil("200",dto.toString()+ " Added",null);
+    }
 }
