668b5fc9fc2bca28e99bbd6b1a47631916ece4ac
uwanitheekshani <uwanitheekshani@gmail.com>
2023-03-08 00:04:00 +0530

    content update

--- Front_End/controllers/AdminController.js
+++ Front_End/controllers/AdminController.js
@@ -47,22 +47,22 @@ $("#btnAddAdmin").click(function () {
 
 $("#btnALogIn").click(function (){
 
-    // let email = $("#txtAdUsNa").val();
-    // let password = $("#txtAdPass").val();
+    let email = $("#txtAdUsNa").val();
+    let password = $("#txtAdPass").val();
 
-    // $.ajax({
-    //     url: baseURL+"admin?email="+email,
-    //     method: "get",
-    //     dataType:"json",
-    //     success: function (res) {
-    //         if (res.data.password==password){
-    //             Swal.fire({
-    //                 position: 'center',
-    //                 icon: 'success',
-    //                 title: "Login Successfully",
-    //                 showConfirmButton: false,
-    //                 timer: 1500
-    //             });
+    $.ajax({
+        url: baseURL+"admin?email="+email,
+        method: "get",
+        dataType:"json",
+        success: function (res) {
+            if (res.data.password==password){
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: "Login Successfully",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
                 $("#admindashMain1").css('display','block');
                 $("#admindashMain2").css('display','block');
                 $("#adminDashLogin").css('display','none');
@@ -78,28 +78,28 @@ $("#btnALogIn").click(function (){
                 $("#mainh").css('display','none');
                 $("#headContent").css('display','none');
 
-    //         }else {
-    //             Swal.fire({
-    //                 position: 'center',
-    //                 icon: 'error',
-    //                 title: "Invalid email or password",
-    //                 showConfirmButton: false,
-    //                 timer: 1500
-    //             });
-    //         }
-    //         clearAdminLoginTextFields();
-    //     },
-    //     error:function(error){
-    //         var jsObject=JSON.parse(error.responseText);
-    //         Swal.fire({
-    //             position: 'center',
-    //             icon: 'error',
-    //             title: "Invalid email or password",
-    //             showConfirmButton: false,
-    //             timer: 1500
-    //         });
-    //     }
-    // });
+            }else {
+                Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: "Invalid email or password",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+            clearAdminLoginTextFields();
+        },
+        error:function(error){
+            var jsObject=JSON.parse(error.responseText);
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: "Invalid email or password",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+    });
 });
 
 function clearAdminLoginTextFields() {
